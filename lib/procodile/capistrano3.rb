bc33f9dcb7473e2897de2851dcba04bd55ca0f0d
Nestor Walker <naw4t@virginia.edu>
2016-10-21 11:51:36 -0400

    refactor and make compatible with rvm

--- lib/procodile/capistrano3.rb
+++ lib/procodile/capistrano3.rb
@@ -1,38 +1,45 @@
+namespace :load do
+  task :defaults do
+    set :rvm_map_bins, fetch(:rvm_map_bins, []).push('procodile')
+
+   if binary = fetch(:procodile_binary, nil)
+     SSHKit.config.command_map[:procodile] = binary
+   end
+   if user = fetch(:procodile_user, nil)
+     SSHKit.config.command_map.prefix[:procodile].push("sudo -u #{user}")
+   end
+  end
+end
 namespace :procodile do
+
   desc 'Start procodile processes'
   task :start do
     on roles(fetch(:procodile_roles, [:app])) do
-      execute procodile_command(:start)
+      execute :procodile, procodile_args(:start)
     end
   end
 
   desc 'Stop procodile processes'
   task :stop do
     on roles(fetch(:procodile_roles, [:app])) do
-      execute procodile_command(:stop)
+      execute :procodile, procodile_args(:stop)
     end
   end
 
   desc 'Restart procodile processes'
   task :restart do
     on roles(fetch(:procodile_roles, [:app])) do
-      execute procodile_command(:restart)
+      execute :procodile, procodile_args(:restart)
     end
   end
 
   after 'deploy:finished', "procodile:restart"
 
-  def procodile_command(command, options = "")
-    binary = fetch(:procodile_binary, 'procodile')
+  def procodile_args(command, options = "")
     if processes = fetch(:processes, nil)
       options = "-p #{processes} " + options
     end
-    command = "#{binary} #{command} -r #{current_path} #{options}"
-    if user = fetch(:procodile_user, nil)
-      "sudo -u #{user} #{command}"
-    else
-      command
-    end
+    "#{command} -r #{current_path} #{options}"
   end
 
 end
